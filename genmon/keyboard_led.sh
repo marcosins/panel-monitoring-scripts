85e85e843230a9dc46630b5a0d3b3937e7ed03ce
Marco <nervloverei@googlemail.com>
2023-03-17 18:09:22 -0300

    feat(genmon): add customizable wasily keyboard symbols

--- genmon/keyboard_led.sh
+++ genmon/keyboard_led.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/env bash
 
 # configuration
-ON_COLOR_CAPS="#C1FFD7"
-ON_COLOR_NUM="#94B3FD"
-ON_COLOR_SCROLL="#FFF89A"
-CLICK="xfce4-keyboard-settings"
+readonly ON_COLOR_CAPS="#C1FFD7"
+readonly ON_COLOR_NUM="#94B3FD"
+readonly ON_COLOR_SCROLL="#FFF89A"
+readonly SYMBOL_CAPS="CAPS" # 🄰
+readonly SYMBOL_NUM="NUMS" # 🄽
+readonly SYMBOL_SCROLL="SCRL" # 🅂
+readonly CLICK="xfce4-keyboard-settings"
 
 # code
 STATE=($(xset q | grep Caps\ Lock | awk '{print $4" "$8" "$12}'))
 
-CAPS=$([[ ${STATE[0]} == "on" ]] && \
-  echo -e "<span foreground='black' background='$ON_COLOR_CAPS'> 🄰 </span>" || \
-  echo -e " 🄰 ")
+CAPS=$([[ ${STATE[0]^^} == "ON" ]] && \
+  echo -e "<span foreground='black' background='$ON_COLOR_CAPS'> <b>$SYMBOL_CAPS</b> </span>" || \
+  echo -e " $SYMBOL_CAPS ")
 
-NUM=$([[ ${STATE[1]} == "on" ]] && \
-  echo -e "<span foreground='black' background='$ON_COLOR_NUM'> 🄽 </span>" || \
-  echo -e " 🄽 ")
+NUM=$([[ ${STATE[1]^^} == "ON" ]] && \
+  echo -e "<span foreground='black' background='$ON_COLOR_NUM'> <b>$SYMBOL_NUM</b> </span>" || \
+  echo -e " $SYMBOL_NUM ")
 
-SCROLL=$([[ ${STATE[2]} == "on" ]] && \
-  echo -e "<span foreground='black' background='$ON_COLOR_SCROLL'> 🅂 </span>" || \
-  echo " 🅂 ")
+SCROLL=$([[ ${STATE[2]^^} == "ON" ]] && \
+  echo -e "<span foreground='black' background='$ON_COLOR_SCROLL'> <b>$SYMBOL_SCROLL</b> </span>" || \
+  echo " $SYMBOL_SCROLL ")
 
 # genmon
 echo -e "<txt>$CAPS$NUM$SCROLL</txt>"
-echo -e "<tool><b>Keyboard indicators:</b>
- 🄰 CAPS
- 🄽 NUM
- 🅂 SCROLL</tool>"
+echo -e "<tool><big><b>Keyboard indicators</b></big>
+ $SYMBOL_CAPS is ${STATE[0]^^}
+ $SYMBOL_NUM is ${STATE[1]^^}
+ $SYMBOL_SCROLL is ${STATE[2]^^}</tool>"
 echo -e "<txtclick>$CLICK</txtclick>"
 
 exit 0
